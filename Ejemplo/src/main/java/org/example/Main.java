9f565a733b58cb4a5c1558cf78177bb6ccc28c9f
brunopistarino <brunopistarino@gmail.com>
2024-06-24 10:20:35 -0300

    avances

--- Ejemplo/src/main/java/org/example/Main.java
+++ Ejemplo/src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ejercicio3();
+        ejercicio4();
+    }
+
+    private static void ejercicio3() {
+        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future3 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future4 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+
+        CompletableFuture<Void> tareas = CompletableFuture.allOf(future1, future2, future3, future4);
+
+        tareas.thenRun(() -> {
+            try {
+                int sum = future1.get() + future2.get() + future3.get() + future4.get();
+                System.out.println("La suma de los 4 valores es: " + sum);
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }).join();
+    }
+
+    private static void ejercicio4() {
+        CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future2 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future3 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+        CompletableFuture<Integer> future4 = CompletableFuture.supplyAsync(Main::generarNumeroAleatorioYDormir);
+
+        CompletableFuture<Object> firstCompletedFuture = CompletableFuture.anyOf(future1, future2, future3, future4);
+
+        firstCompletedFuture.thenAccept(result -> {
+            System.out.println("El valor del primer futuro completado es: " + result);
+        }).join();
+    }
+
+    private static int generarNumeroAleatorioYDormir() {
+        int randomValue = ThreadLocalRandom.current().nextInt(100, 501);
+        try {
+            Thread.sleep(randomValue);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return randomValue;
     }
 }
--- Ejemplo/src/main/java/org/example/StreamBenchmark.java
+++ Ejemplo/src/main/java/org/example/StreamBenchmark.java
@@ -1,25 +1,52 @@
 package org.example;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+@State(Scope.Benchmark)
 public class StreamBenchmark {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(StreamBenchmark.class.getSimpleName())
+                .warmupIterations(5)
+                .measurementIterations(10)
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    @Setup(Level.Trial)
+    public void setUp() {
         int size = 10000000;
         Random random = new Random();
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             list.add(random.nextInt(50000) + 1);
         }
+    }
 
-        List<Integer> resultSecuencial = list.stream()
+    @Benchmark
+    public void resultSecuencial(Blackhole bh) {
+        bh.consume(list.stream()
                 .filter(n -> n % 2 == 0)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+    }
 
-        List<Integer> resultParalelo = list.parallelStream()
+    @Benchmark
+    public void resultParalelo(Blackhole bh) {
+        bh.consume(list.parallelStream()
                 .filter(n -> n % 2 == 0)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 }
